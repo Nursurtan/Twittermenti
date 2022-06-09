c8b919c98b3b9353814a0faa4bca2006e9d7b1f6
Nursurtan <arapovnur23@gmail.com>
2022-06-09 08:03:58 +0700

    Added Swifter Framework setting

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import SwifteriOS
+
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "LTReJyreyyuz4SWjVABuA1WKI", consumerSecret: "ajf107xfd6869heCG8Ptn8iztfFHBWPIxLkFGvsmoEwI7MKZcz")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        swifter.searchTweet(using: "@Apple") { (results, metadata) in
+            print(results)
+        } failure: { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
+
         
     }
 
