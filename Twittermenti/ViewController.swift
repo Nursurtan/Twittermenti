5ec165588a8ce87a93e70637b9371e2968f5d499
Nursurtan <arapovnur23@gmail.com>
2022-06-09 08:45:59 +0700

    Added the ability to receive real-time Twitter Data.

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -16,17 +16,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
+    
+    
+//    let swifter = Swifter(consumerKey: “9F4Rnej4pW39Dj1FvnFSsneQW”, consumerSecret: “tInTcFmb1AXiLEZyyxjpRfvnzbVqqFNwB1Zu4YFFJJ6hZGf6Qb”)
+    
     let swifter = Swifter(consumerKey: "LTReJyreyyuz4SWjVABuA1WKI", consumerSecret: "ajf107xfd6869heCG8Ptn8iztfFHBWPIxLkFGvsmoEwI7MKZcz")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swifter.searchTweet(using: "@Apple") { (results, metadata) in
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
             print(results)
-        } failure: { (error) in
+        }) { (error) in
             print("There was an error with the Twitter API Request, \(error)")
         }
 
+//        swifter.searchTweet(using: <#T##String#>, geocode: <#T##String?#>, lang: <#T##String?#>, locale: <#T##String?#>, resultType: <#T##String?#>, count: <#T##Int?#>, until: <#T##String?#>, sinceID: <#T##String?#>, maxID: <#T##String?#>, includeEntities: <#T##Bool?#>, callback: <#T##String?#>, tweetMode: <#T##TweetMode#>, success: <#T##Swifter.SearchResultHandler?##Swifter.SearchResultHandler?##(JSON, _ searchMetadata: JSON) -> Void#>, failure: <#T##Swifter.FailureHandler##Swifter.FailureHandler##(_ error: Error) -> Void#>)
+        
+        
         
     }
 
